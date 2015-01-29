390b4ac2c687577af47931c04f91e331d9071488
Timofey Lyzhenkov <timofeylyzhenkov@gmail.com>
2015-01-29 11:03:36 -0500

    init

---
+++ common/config/main.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+			'class' => 'yii\caching\FileCache',
+		],
+		'urlManager' => [
+			'enablePrettyUrl' => true,
+			'showScriptName' => false, 
+			'rules' => [
+				// your rules go here
+			],
+		]
+    ],
+];
