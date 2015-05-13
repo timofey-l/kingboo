8cf9fe292286664b4757d0e079c04cc38c23e477
Timofey Lyzhenkov <timofeylyzhenkov@gmail.com>
2015-05-13 15:15:30 +0300

    Правка ошибки выдачи виджета

--- partner/web/js/widget/widget.js
+++ partner/web/js/widget/widget.js
@@ -1,5 +1,24 @@
 var bw_lang_vars = {
     ru: {
+        datepicker: {
+            closeText: 'Закрыть',
+            prevText: '&#x3C;Пред',
+            nextText: 'След&#x3E;',
+            currentText: 'Сегодня',
+            monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь',
+                'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+            monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн',
+                'Июл','Авг','Сен','Окт','Ноя','Дек'],
+            dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+            dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
+            dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+            weekHeader: 'Нед',
+            dateFormat: 'dd.mm.yy',
+            firstDay: 1,
+            isRTL: false,
+            showMonthAfterYear: false,
+            yearSuffix: ''
+        },
         messages: {
             'lang_dateFrom_label': "Дата заезда",
             'lang_dateTo_label': "Дата отъезда",
@@ -11,6 +30,7 @@ var bw_lang_vars = {
         }
     },
     en: {
+        //datepicker:
         messages: {
             'lang_dateFrom_label': "Check-in date",
             'lang_dateTo_label': "Check-out date",
