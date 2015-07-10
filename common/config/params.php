c0fcf2bf0b6ae0ec1064ed52db42f7e3661c1780
Timofey Lyzhenkov <timofeylyzhenkov@gmail.com>
2015-07-10 19:37:24 +0300

    К админам добавлен адрес mn@itdesign.ru

--- common/config/params.php
+++ common/config/params.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'adminEmail' => 'timofeylyzhenkov@gmail.com',
+    'adminEmail' => ['timofeylyzhenkov@gmail.com', 'mn@itdesign.ru'],
     'email.from' => 'no-reply@king-boo.com',
     'supportEmail' => 'timofeylyzhenkov@gmail.com',
     'user.passwordResetTokenExpire' => 3600,
