31e07bb7f4e000af20bf4345241a593be2864e42
Timofey Lyzhenkov <timofeylyzhenkov@gmail.com>
2015-07-11 16:18:30 +0300

    Стили почтовых сообщений

--- common/config/params.php
+++ common/config/params.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'adminEmail' => ['timofeylyzhenkov@gmail.com' => 'Timofey Lyzhenkov'],
-    'email.from' => 'no-reply@king-boo.com',
+    'email.from' => ['no-reply@king-boo.com' => 'king-boo.com'],
     'supportEmail' => 'timofeylyzhenkov@gmail.com',
     'user.passwordResetTokenExpire' => 3600,
 ];
--- common/mail/_order-html.php
+++ common/mail/_order-html.php
@@ -9,57 +9,53 @@ $translate_category = 'mails_order';
 /** @var \common\models\Currency $currency */
 $currency = $order->hotel->currency;
 
-$this->registerCss("
-    .orderTable {
+$td = "border: 0px solid #eeeeee; padding: 5px;";
 
-    }
-    .orderTable td {
-        padding: 0.2em;
-        border: 1px solid #cccccc;
-    }
+$td_label = "vertical-align: top; text-align:right; color: #666; border-right: 0px; padding-right: 2px;";
 
-
-", [], 'table')
+$td_value = "vertical-align: top; text-align:left; font-weight: bold; border-left: 0px; padding-left: 2px;";
 
 ?>
 
-<table class="orderTable">
+<table style="border-collapse: collapse;">
     <tr>
-        <td colspan="2"><?= \Yii::t($translate_category, 'Check-in date', [], $lang) ?></td>
-        <td><?= (new \DateTime($order->dateFrom))->format(\Yii::t($translate_category,'d.m.Y', [], $lang)) ?></td>
-        <td rowspan="2">
-            (<?= \Yii::t($translate_category, 'Nights: {n}', ['n' => $order->nights], $lang) ?>)
+        <td style="<?= $td . $td_label ?>"
+            style="<?= $td ?>"><?= \Yii::t($translate_category, 'Check-in date', [], $lang) ?></td>
+        <td style="<?= $td . $td_value ?>"
+            style="<?= $td ?>"><?= (new \DateTime($order->dateFrom))->format(\Yii::t($translate_category, 'd/m/Y', [], $lang)) ?></td>
+        <td style="<?= $td ?>" style="<?= $td ?>" rowspan="2">
+            <?= \Yii::t($translate_category, 'Nights: {n}', ['n' => $order->nights], $lang) ?>
         </td>
-        <td>
+        <td style="<?= $td . $td_label ?>">
             <?= \Yii::t($translate_category, 'Order sum', [], $lang) ?>
         </td>
-        <td>
+        <td style="<?= $td . $td_value ?>">
             <?= $currency->getFormatted($order->sum) ?>
         </td>
     </tr>
     <tr>
-        <td colspan="2"><?= \Yii::t($translate_category, 'Check-out date', [], $lang) ?></td>
-        <td><?= (new \DateTime($order->dateTo))->format(\Yii::t($translate_category,'d.m.Y', [], $lang)) ?></td>
-        <td>
+        <td style="<?= $td . $td_label ?>"><?= \Yii::t($translate_category, 'Check-out date', [], $lang) ?></td>
+        <td style="<?= $td . $td_value ?>"><?= (new \DateTime($order->dateTo))->format(\Yii::t($translate_category, 'd/m/Y', [], $lang)) ?></td>
+        <td style="<?= $td . $td_label ?>">
             <?= \Yii::t($translate_category, 'Pay sum', [], $lang) ?>
         </td>
-        <td>
+        <td style="<?= $td . $td_value ?>">
             <?= $currency->getFormatted($order->pay_sum) ?>
         </td>
     </tr>
     <tr>
-        <td>
+        <td style="<?= $td . $td_label ?>">
             <?= \Yii::t($translate_category, 'Hotel', [], $lang) ?>
         </td>
-        <td colspan="5">
-            <?= $order->hotel->{'title_'.$lang} ?>
+        <td style="<?= $td . $td_value ?>" colspan="4">
+            <?= $order->hotel->{'title_' . $lang} ?>
         </td>
     </tr>
     <tr>
-        <td>
+        <td style="<?= $td . $td_label ?>">
             <?= \Yii::t($translate_category, 'Contact information', [], $lang) ?>
         </td>
-        <td colspan="5">
+        <td style="<?= $td . $td_value ?>" colspan="4">
             <?= \common\components\ListAddressType::getTitle($order->contact_address, $lang) ?>
             <?= \yii\helpers\Html::encode($order->contact_name) ?> <?= \yii\helpers\Html::encode($order->contact_surname) ?>
             <br/>
@@ -69,18 +65,18 @@ $this->registerCss("
         </td>
     </tr>
 </table>
-<?= \Yii::t($translate_category, 'Numbers', [], $lang) ?>:
-<table>
-    <?php foreach($order->orderItems as $index => $item): ?>
+<?= \Yii::t($translate_category, 'Rooms', [], $lang) ?>:
+<table style="border-collapse: collapse;">
+    <?php foreach ($order->orderItems as $index => $item): ?>
         <?php /** @var \common\models\OrderItem $item */ ?>
         <tr>
-            <td rowspan="2"><?= $index ?></td>
-            <td><?= $item->room->{'title_' . $lang} ?></td>
-            <td><?= \Yii::t($translate_category, 'Sum', [], $lang) ?></td>
-            <td><?= $currency->getFormatted($item->sum) ?></td>
+            <td style="<?= $td."font-size: 200%; font-weight: bold" ?>" rowspan="2"><?= $index+1 ?></td>
+            <td style="<?= $td ?>"><?= $item->room->{'title_' . $lang} ?></td>
+            <td style="<?= $td . $td_label ?>"><?= \Yii::t($translate_category, 'Sum', [], $lang) ?></td>
+            <td style="<?= $td . $td_value ?>"><?= $currency->getFormatted($item->sum) ?></td>
         </tr>
         <tr>
-            <td colspan="3">
+            <td style="<?= $td ?>" colspan="3">
                 <?= \Yii::t($translate_category, 'Adults: <b>{n}</b>', ['n' => $item->adults], $lang) ?>
                 &nbsp;&nbsp;&nbsp;
                 <?= \Yii::t($translate_category, 'Children 7-12 y.o.: <b>{n}</b>', ['n' => $item->children], $lang) ?>
@@ -90,4 +86,3 @@ $this->registerCss("
         </tr>
     <?php endforeach; ?>
 </table>
-
