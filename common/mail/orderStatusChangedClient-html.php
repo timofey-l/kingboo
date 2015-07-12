407d38a68d00f385d87ebf8b3abb04a6efc268af
Timofey Lyzhenkov <timofeylyzhenkov@gmail.com>
2015-07-12 13:48:46 +0300

    Параметры перевода

--- common/mail/orderStatusChangedClient-html.php
+++ common/mail/orderStatusChangedClient-html.php
@@ -13,7 +13,7 @@ if (!isset($lang)) {
 <div>
     <p><?= \Yii::t('mails_order', 'Hello, {name}!', ['name' => $order->contact_name . ' ' . $order->contact_surname], $local) ?></p>
 
-    <p><?= \Yii::t('mails_order', 'Status of your order #{n} was changed to "{new_status}".', ['new_status' => \common\models\Order::getOrderStatusTitle($order->status)], $local) ?></p>
+    <p><?= \Yii::t('mails_order', 'Status of your order #{n} was changed to "{new_status}".', ['new_status' => \common\models\Order::getOrderStatusTitle($order->status), 'n' => $order->number], $local) ?></p>
 
     <p><?= \Yii::t('mails_order', 'Best regards, team of king-boo.com', [], $local) ?></p>
 
--- common/mail/orderStatusChangedPartner-html.php
+++ common/mail/orderStatusChangedPartner-html.php
@@ -13,7 +13,7 @@ if (!isset($lang)) {
 <div>
     <p><?= \Yii::t('mails_order', 'Hello!', [], $local) ?></p>
 
-    <p><?= \Yii::t('mails_order', 'Status of order #{n} was changed to "{new_status}".', ['new_status' => \common\models\Order::getOrderStatusTitle($order->status)], $local) ?></p>
+    <p><?= \Yii::t('mails_order', 'Status of order #{n} was changed to "{new_status}".', ['new_status' => \common\models\Order::getOrderStatusTitle($order->status), 'n' => $order->partner_number], $local) ?></p>
 
     <p><?= \Yii::t('mails_order', 'You can open order to manage it by <a href="{url}">clicking here</a>.', ['url' => \yii\helpers\Url::to('@partner_web') . '/orders/view?id=' . $order->id]) ?></p>
 
