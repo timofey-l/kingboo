1b53bc1774e2460844219c1229a2c28eb0a91468
MaxNesvit <mn@itdesign.ru>
2015-09-25 20:42:32 +0400

    Исправлен доступ к favicon

--- partner/adminTheme2/layouts/main.php
+++ partner/adminTheme2/layouts/main.php
@@ -29,7 +29,7 @@ if (in_array(Yii::$app->controller->action->id, ['login', 'error', 'signup', 're
     <head>
         <meta charset="<?= Yii::$app->charset ?>"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" href="favicon.ico" type="image/x-icon">
+        <link rel="icon" href="/favicon.ico" type="image/x-icon">
         <?= Html::csrfMetaTags() ?>
         <title><?= Html::encode($this->title) ?></title>
         <?php $this->head() ?>
