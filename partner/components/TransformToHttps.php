cb23eb436bf3660d298d2b906b1733c9fe816b12
MaxNesvit <mn@itdesign.ru>
2015-09-27 18:20:08 +0400

    Совмещение https и http в partner 2.7

--- partner/components/TransformToHttps.php
+++ partner/components/TransformToHttps.php
@@ -11,6 +11,7 @@ class TransformToHttps extends Component {
 		curl_setopt($s, CURLOPT_URL, $url); 
 		curl_setopt($s, CURLOPT_RETURNTRANSFER, true); 
 		curl_setopt($s, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($s, CURLOPT_FRESH_CONNECT, true);
 		$page = curl_exec($s);
 
 		curl_close($s);
@@ -22,35 +23,29 @@ class TransformToHttps extends Component {
 
 		if (!$page) {
 			$response->content = '';
-			$response->send();
-			return;
-		}
-		if ($script) {
+		} elseif ($script) {
 			$response->content = $page;
-			$response->send();
-			return;
+		} else {
+			$page = preg_replace_callback("#href=\"([^\"]+?)\"#is", function($matches) {
+				$s = self::replace($matches);
+				return "href=\"$s\"";
+			}, $page);
+
+			$page = preg_replace_callback("#src=\"([^\"]+?)\"#is", function($matches) {
+				$s = self::replace($matches);
+				return "src=\"$s\"";
+			}, $page);
+
+			$response->content = $page;
 		}
 
-		$page = preg_replace_callback("#href=\"([^\"]+?)\"#is", function($matches) {
-			$s = self::replace($matches);
-			return "href=\"$s\"";
-		}, $page);
-
-		$page = preg_replace_callback("#src=\"([^\"]+?)\"#is", function($matches) {
-			$s = self::replace($matches);
-			return "src=\"$s\"";
-		}, $page);
-
-		$response->content = $page;
 		$response->send();
 	}
 
 	private static function replace($matches) {
 		$base = \Yii::$app->params['partnerProtocol'] . '://' . \Yii::$app->params['partnerDomain'];
-		if (strpos($matches[1], '/assets/') === 0) {
+		if (strpos($matches[1], '/assets/') === 0 || strpos($matches[1], '/') === 0) {
 			$s = $base . '/site/transform-to-https?script=1&url=http://' . \Yii::$app->params['mainDomain'] . $matches[1];
-		} elseif (strpos($matches[1], '/') === 0) {
-			$s = $base . $matches[1];
 		} elseif (strpos($matches[1], 'mailto:') === 0 || $matches[1]{0} == '{') {
 			$s = $matches[1];
 		} else {
