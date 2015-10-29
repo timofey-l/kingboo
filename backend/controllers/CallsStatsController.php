3b7232bcd154239dccdb568abb8373f9c7e9f2cd
Timofey Lyzhenkov <timofeylyzhenkov@gmail.com>
2015-10-29 17:15:00 +0000

    Call stats fixes

--- backend/controllers/CallsStatsController.php
+++ backend/controllers/CallsStatsController.php
@@ -30,6 +30,7 @@ class CallsStatsController extends \yii\web\Controller
                 $user->setPassword($password);
                 $user->generateAuthKey();
                 if ($user->save()) {
+                    $user->sendConfirmEmail();
                     $model->date = date(\DateTime::ISO8601);
                     $model->save(false);
                     var_dump($model->errors);
